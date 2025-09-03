47582a0b043e006e030a9c1af65b138186c8daa6
Ajay Verma <an8295222@gmail.com>
2025-09-03 17:19:48 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from "./Pages/Home"
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
+import ScrollToTop from './Components/ScrollToTop'
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
       <div>
         <Navbar/>
         <BrowserRouter>
+        <ScrollToTop/>
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
